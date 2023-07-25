e3d33f87d517132200526cfa241a8dee89c4b4be
Krishna Babuji <krishnababuji@baekho.local>
2023-07-26 01:06:57 +0530

    initial commit: Add biryaniFactory and omeletteBuilder

---
+++ biriyaniFactory.py
@@ -0,0 +1,42 @@
+class Biriyani:
+    """Creates a biriyani object."""
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+    
+    def print(self):
+        print(self.ingredients)
+
+class BiriyaniFactory:
+    """Creates a biriyani from one of the preset options. 
+        We can quickly create any of the following biriyanis [chicken, egg, paneer, and mutton biriyani].
+
+        NOTE: Customers cannot make modifications to any of the biryanis nor can they 
+        know what spices exactly went into the making of a biryani.
+   """
+    
+    def createChickenBiriyani(self):
+        """Creates a chicken biriyani with rice, spices, and chicken."""
+        ingredients = ["rice", "spices", "chicken"]
+        return Biriyani(ingredients)
+    
+    
+    def createEggBiriyani(self):
+        """Creates a egg biriyani with rice, spices, and egg."""
+        ingredients = ["rice", "spices", "egg"]
+        return Biriyani(ingredients)
+    
+    
+    def createPaneerBiriyani(self):
+        """Creates a paneer biriyani with rice, spices, and vegetables."""
+        ingredients = ["rice", "spices", "paneer"]
+        return Biriyani(ingredients)
+    
+    
+    def createMuttonBiriyani(self):
+        """Creates a mutton biriyani with rice, spices, mutton."""
+        ingredients = ["rice", "spices", "mutton"]
+        return Biriyani(ingredients)
+
+if __name__ == '__main__':
+    biriyaniFactory = BiriyaniFactory()
+    biriyaniFactory.createPaneerBiriyani().print()
---
+++ omeletteBuilder.py
@@ -0,0 +1,47 @@
+class Omelette:
+    """Omelette object; Add your choice of cheese, fillings, herbs."""
+    def __init__(self):
+        self.cheese: str = ""
+        self.fillings: list = []
+        self.herbs: list = []
+    
+    def setCheese(self, cheese: str):
+        self.cheese = cheese
+    
+    def setFillings(self, fillings: list):
+        self.fillings = fillings
+    
+    def setHerbs(self, herbs: list):
+        self.herbs = herbs
+
+    def print(self):
+        print(f'Omelette has cheese: {self.cheese}, fillings: {",".join(self.fillings)}, and the following herbs: {",".join(self.herbs)}')
+
+class OmeletteBuilder:
+    """Build any valid omelette, that is custom to your wants."""
+    def __init__(self):
+        self.omelette = Omelette()
+
+    def addCheese(self, cheese: str):
+        self.omelette.setCheese(cheese)
+        return self
+    
+    def addFillings(self, fillings: list):
+        self.omelette.setFillings(fillings)
+        return self
+    
+    def addHerbs(self, herbs: list):
+        self.omelette.setHerbs(herbs)
+        return self
+    
+    def build(self):
+        return self.omelette
+    
+
+if __name__ == '__main__':
+    customOmelette = OmeletteBuilder() \
+        .addCheese('Feta') \
+        .addFillings(['Mushroom', 'Spinach']) \
+        .build()
+    customOmelette.print()
+    
